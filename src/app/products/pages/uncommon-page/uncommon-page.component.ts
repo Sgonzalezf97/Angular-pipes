4c11f66fae114d088bfe21e722b17687a90bf8b5
Sebastian Gonzalez <jusegofo@gmail.com>
2024-01-07 23:24:39 -0500

    comentarios

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -23,7 +23,8 @@ export class UncommonPageComponent {
 
   //i18nPlural
   public clients:string[]=['Maria','Pedro','Marcos','Juan','Marcela','Camilo','Cristian',]
-  public clientsMap={
+
+  public clientsMap={ //este map nos sirve para cuando se tenga el valor de ese texto
     '=0':'No tenemos ningún cliente esperando.',
     '=1':'tenemos un cliente esperando.',
     'other':'tenemos # clientes esperando'
@@ -33,4 +34,15 @@ export class UncommonPageComponent {
   deleteClient():void{
     this.clients.shift();
   }
+
+  //slice
+  // slice:0:2 corta los valores del arreglo desde el 0 al 2
+
+  //keyValue  --> este sirve para poder iterar sobre las propiedades del objeto conviertiendolas en clave valor
+  public person={
+    name:'Sebastian',
+    age:36,
+    address:'Bogotá, Colombia'
+  }
+
 }
