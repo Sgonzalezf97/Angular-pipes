00597c51ec7f9ec4fce0c16f04d0b9c851e9fa89
Sebastian Gonzalez <jusegofo@gmail.com>
2024-01-07 23:24:09 -0500

    plural pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrl: './uncommon-page.component.css'
 })
 export class UncommonPageComponent {
+  //i18Select
+  public name: string='Sebastian';
+  public gender: 'male'|'female'='male';
 
+  //objeto map con las opciones del valor
+  public invitationMap={
+    'male':'invitarlo',
+    'female':'invitarla'
+  }
+
+  changeClient():void{
+    this.name='Melissa';
+    this.gender='female';
+  }
+
+  //i18nPlural
+  public clients:string[]=['Maria','Pedro','Marcos','Juan','Marcela','Camilo','Cristian',]
+  public clientsMap={
+    '=0':'No tenemos ningún cliente esperando.',
+    '=1':'tenemos un cliente esperando.',
+    'other':'tenemos # clientes esperando'
+
+  }
+
+  deleteClient():void{
+    this.clients.shift();
+  }
 }
